09ed899a01c6abd815a240b8824eb9e1efba2627
Marc <mdd_13@hotmail.com>
2012-02-17 14:19:00 +0100

    Add Buildings in level

--- Game/src/Level.java
+++ Game/src/Level.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+
 
 public class Level {
 	public int w;
 	public int h;
+	private ArrayList <Building> builds;
 	
 	public Level(int w, int h){
 		this.w = w/8;
 		this.h = h/8;
+	}
+	
+	public void AddBuilding(int x, int y, GameSource game){
+		builds.add(new Building(x,y,game));
 	}
 }
