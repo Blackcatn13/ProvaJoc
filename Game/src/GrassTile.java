bde4cb38fd5587994c198bfed309173a6b3a348b
Marc <mdd_13@hotmail.com>
2012-02-17 17:34:10 +0100

    Modified the Render function

--- Game/src/GrassTile.java
+++ Game/src/GrassTile.java
@@ -7,9 +7,9 @@ public class GrassTile extends Tile{
 	
 	public void render(Screen screen, int x, int y){
 		int col = Color.get(-1,50,150,0);
-		screen.render(x, y, 640, col, 0);
-		screen.render(x+8, y, 641, col, 0);
-		screen.render(x, y+8, 672, col, 0);
-		screen.render(x+8, y+8, 673, col, 0);
+		screen.render(x, y, 704, col, 0);
+		screen.render(x+8, y, 705, col, 0);
+		screen.render(x, y+8, 736, col, 0);
+		screen.render(x+8, y+8, 737, col, 0);
 	}
 }
