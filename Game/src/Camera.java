88d5eb7e083937b5de607c6f7439d790e45b26de
Marc <mdd_13@hotmail.com>
2012-02-17 18:05:08 +0100

    Add camera move with mouse

--- Game/src/Camera.java
+++ Game/src/Camera.java
@@ -2,11 +2,13 @@
 public class Camera {
 	int x,y;
 	InputHandler input;
+	MouseHandler input1;
 	
-	public Camera(InputHandler Input){
+	public Camera(InputHandler Input, MouseHandler Input1){
 		x = 80;
 		y = 56;
 		input = Input;
+		input1 = Input1;
 	}
 	
 	public void tick(){
@@ -14,5 +16,10 @@ public class Camera {
 		if(input.up.down && y > 56) y--;
 		if(input.left.down && x > 80) x--;
 		if(input.right.down && x < 160) x++;
+		if(input1.x > 155 && x < 160) x++;
+		if(input1.x < 5 && x > 80) x--;
+		if(input1.y > 115 && y < 120) y++;
+		if(input1.y < 5 && y > 56) y--;
+		
 	}
 }
