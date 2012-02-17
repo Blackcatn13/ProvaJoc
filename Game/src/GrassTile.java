fe4a17a28ee591d8ab149922bb55e794eb9a0dd1
Marc <mdd_13@hotmail.com>
2012-02-17 19:19:08 +0100

    Changed the render function

--- Game/src/GrassTile.java
+++ Game/src/GrassTile.java
@@ -5,8 +5,13 @@ public class GrassTile extends Tile{
 		super(id);
 	}
 	
-	public void render(Screen screen, int x, int y){
-		int col = Color.get(-1,50,150,0);
+	public void render(Screen screen, int x, int y, boolean possible){
+		int col;
+		if(possible){
+			col = Color.get(-1,255,150,0);
+		}else{
+			col = Color.get(-1,50,150,0);
+		}
 		screen.render(x, y, 704, col, 0);
 		screen.render(x+8, y, 705, col, 0);
 		screen.render(x, y+8, 736, col, 0);
