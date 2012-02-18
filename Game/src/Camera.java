ddaa3e540104ee4806ee42261e5e271c66cf5066
Marc <mdd_13@hotmail.com>
2012-02-18 15:39:00 +0100

    Camera working with a big level

--- Game/src/Camera.java
+++ Game/src/Camera.java
@@ -1,8 +1,8 @@
 
 public class Camera {
-	int x,y;
-	InputHandler input;
-	MouseHandler input1;
+	public int x,y;
+	private InputHandler input;
+	private MouseHandler input1;
 	
 	public Camera(InputHandler Input, MouseHandler Input1){
 		x = 80;
@@ -12,13 +12,13 @@ public class Camera {
 	}
 	
 	public void tick(){
-		if(input.down.down && y < 120) y++;
+		if(input.down.down && y < 183) y++;
 		if(input.up.down && y > 56) y--;
 		if(input.left.down && x > 80) x--;
-		if(input.right.down && x < 160) x++;
-		if(input1.x > 155 && x < 160) x++;
+		if(input.right.down && x < 239) x++;
+		if(input1.x > 155 && x < 239) x++;
 		if(input1.x < 5 && x > 80) x--;
-		if(input1.y > 100 && y < 120) y++;
+		if(input1.y > 100 && y < 183) y++;
 		if(input1.y < 5 && y > 56) y--;
 		
 	}
