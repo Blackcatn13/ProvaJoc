cee33469706715739021afb075a1245a8e7dc3f3
Marc <mdd_13@hotmail.com>
2012-02-19 11:11:24 +0100

    Delete needless comments

--- Game/src/Font.java
+++ Game/src/Font.java
@@ -1,12 +1,4 @@
-// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 03/01/2012 10:42:32
-// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
-// Decompiler options: packimports(3) 
-// Source File Name:   Font.java
 
-
-
-// Referenced classes of package com.mojang.ld22.gfx:
-//            Screen, Color
 
 public class Font
 {
