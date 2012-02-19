0da62167a8c3f2853ea1b3ad25e987df8758bf6c
Marc <mdd_13@hotmail.com>
2012-02-19 10:35:19 +0100

    New class for the soldiers

---
+++ Game/src/Entity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Marc
+ *
+ */
+public class Entity {
+	int x, y, totaltime, actutime;
+	
+	public Entity(int x, int y, int Time){
+		actutime = 0;
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void render(){
+		
+	}
+	
+}
